139a78f01fef11ffd91ef130d5041fb89c2a7764
TahielLC <sentey.44@gmail.com>
2025-03-27 20:12:49 -0300

    1 y 2 compeltos

--- palabras.c
+++ palabras.c
@@ -4,6 +4,40 @@
 int main(int argc, char *argv[])
 {
     // Agregar código aquí.
+    char frase[100];
+    char buffer[100];
+    char letra;
+    char letra2;
+    char palabra[100];
+    int i = 0;
+    while ((letra2 = getchar()) != EOF)
+    {
+        if (letra2 != ' ' && letra2 != '\n')
+        {
+            buffer[i] = letra2;
+            i++;
+        }
+        else
+        {
+
+            buffer[i] = '\0';
+            i = 0;
+            printf("%s\n", buffer);
+            // i++;
+        }
+    }
+
+    // for (int i = 0;  (letra = getchar() != ' '); i++)
+    // {
+    //     if (letra)
+    //     {
+    //         /* code */
+    //     }
+
+    // }
+
+    // printf(buffer);
+    //
 
     // Termina la ejecución del programa.
     exit(EXIT_SUCCESS);
--- sum.c
+++ sum.c
@@ -4,7 +4,26 @@
 int main(int argc, char *argv[])
 {
     // Agregar código aquí.
+    int num = 0;
 
-    // Termina la ejecución del programa.
+    // printf("el resultado es: %d", resu);
+    // // Termina la ejecución del programa.
+    int suma = 0;
+    if (argc > 1)
+    {
+
+        for (int i = 1; i < argc; i++)
+        {
+            suma += atoi(argv[i]);
+        }
+    }
+    else
+    {
+        while (scanf("%d", &num) == 1)
+        {
+            suma += num;
+        }
+    }
+    printf("el resultado es: %d", suma);
     exit(EXIT_SUCCESS);
 }
