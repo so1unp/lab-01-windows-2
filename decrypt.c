522101fc955142dbca9be110d0a6e6d66d4bcbdc
Agustin Palma <aguspalqui@hotmail.com>
2025-03-27 20:23:02 -0300

    update encrypt and decrypt

--- decrypt.c
+++ decrypt.c
@@ -3,53 +3,51 @@
 #include <unistd.h>
 #include <fcntl.h>
 
-#define block_size 8 // tamaño en bytes del bloque
+#define block_size 8 // tamaño del bloque a encriptar
 
-void desencriptar(char *mensaje, int len)
-{
+void desencriptar(char *mensaje, ssize_t len) {
 
     unsigned char temp_buffer[1024]; // aca se guarda el mensaje desencriptado
-
     int j = 0; // cantidad de bytes escritos en temp_buffer
 
-
-    for (int i = block_size - 1; i < len; i += block_size)
-    {                                  // comienza en el ultimo byte (7), es decir en el caracter encriptado
-        temp_buffer[j++] = mensaje[i]; // guarda solo el byte real
+    for (int i = block_size - 1; i < len; i += block_size) { // comienza en el ultimo byte (7), es decir en el caracter encriptado
+        temp_buffer[j++] = mensaje[i];// guarda solo el byte real
     }
 
     write(STDOUT_FILENO, temp_buffer, j); // escribir el mensaje desencriptado
 }
 
-int main(int argc, char *argv[])
-{
-    char mensaje[8];
-    ssize_t bytes_leidos;
-    // Agregar código aquí.
-    if (argc == 1) // leer  desde la entrada estandar e imprimir en salida estandar
-    {
-        while (bytes_leidos > 0)
-        {
-            bytes_leidos = read(STDIN_FILENO, mensaje, sizeof(mensaje));
+int main(int argc, char *argv[]) {
 
+    char mensaje[block_size];
+    ssize_t bytes_leidos;
+
+    // Agregar código aquí.
+    if (argc == 1) { // leer  desde la entrada estandar e imprimir en salida estandar
+        while ((bytes_leidos = read(STDIN_FILENO, mensaje, sizeof(mensaje))) > 0) {
+            if (bytes_leidos % block_size != 0) {
+                // ignorar bloques incompletos
+                continue;
+            }
+            
             desencriptar(mensaje, bytes_leidos);
         }
-    }
-    else if (argc == 2) // leer desde un archivo e imprimir en la salida estandar
-    {
-        int fd = open(argv[1], O_RDONLY);
+    } else if (argc == 2) { // leer desde un archivo e imprimir en la salida estandar
 
-        if (fd == -1)
-        {
+        int fd = open(argv[1], O_RDONLY);
+        if (fd == -1) {
             perror("error al abrir el archivo");
             exit(EXIT_FAILURE);
         }
-        while (bytes_leidos > 0)
-        {
-            bytes_leidos = read(STDIN_FILENO, mensaje, sizeof(mensaje));
+
+        while ((bytes_leidos = read(fd, mensaje, sizeof(mensaje))) > 0) {
+            if (bytes_leidos % block_size != 0) {
+                continue;
+            }
 
             desencriptar(mensaje, bytes_leidos);
         }
+
         close(fd);
     }
     // Termina la ejecución del programa.
--- encrypt.c
+++ encrypt.c
@@ -2,59 +2,56 @@
 #include <stdlib.h>
 #include <unistd.h>
 #include <fcntl.h>
+#include <string.h>
 
 #define block_size 8 // tamaño del bloque a encriptar
 
-void encriptar(int fd, char *mensaje)
-{
-    size_t len = strlen(mensaje);
+void encriptar(int fd, char *mensaje, ssize_t len) {
+
     unsigned char buffer[block_size];
     srand(time(NULL)); //  cambia la semilla cada vez q se ejecuta el programa segun el tiempo (time)
+
     // bucle para encriptar cada caracter
-    for (int i = 0; i < len; i++)
-    {
-        // completar con 7 bytes aleatorios
-        for (int j = 0; j < 7; j++)
-        {
+    for (int i = 0; i < len; i++) {
+
+        for (int j = 0; j < 7; j++) {
+            // completar con 7 bytes aleatorios
             buffer[j] = rand() % 256;
         }
+
         buffer[7] = mensaje[i];
         write(fd, buffer, block_size);
     }
 }
 
-int main(int argc, char *argv[])
-{
-
+int main(int argc, char *argv[]) {
+    
     int fd = STDOUT_FILENO;
-
     // Agregar código aquí.
-    if (argc == 1) // leer  desde la entrada estandar e imprimir en salida estandar
-    {
-        char mensaje[8] = {0}; // Inicializa con ceros
-        ssize_t bytes_leidos = read(STDIN_FILENO, mensaje, sizeof(mensaje));
-        while (bytes_leidos > 0)
-        {
-            encriptar(fd, mensaje);
-            bytes_leidos = read(STDIN_FILENO, mensaje, sizeof(mensaje));
+    if (argc == 1) { // leer  desde la entrada estandar e imprimir en salida estandar
+
+        char mensaje[block_size];
+        ssize_t bytes_leidos;
+
+        while ((bytes_leidos = read(STDIN_FILENO, mensaje, sizeof(mensaje))) > 0) {
+            encriptar(fd, mensaje, bytes_leidos);
         }
-    }
-    else if (argc == 2) // leer desde la terminal y imprimir en la salida estandar
-    {
-        encriptar(fd, argv[1]);
-    }
-    else if (argc == 3) // leer desde la terminal y imprimir en un archivo
-    {
+
+    } else if (argc == 2) { // leer desde la terminal y imprimir en la salida estandar
+        encriptar(fd, argv[1], strlen(argv[1]));
+
+    } else if (argc == 3) {
         fd = open(argv[1], O_WRONLY | O_CREAT | O_TRUNC, 0644); // argv[1] es el nombre del archivo, lo abre en modo escritura con o_wronly, si no existe lo crea
                                                                 // con o_creat y si existe le borra el conteido con o_trunc
-        if (fd == -1)
-        {
+        if (fd == -1) {
             perror("error al abrir el archivo");
             exit(EXIT_FAILURE);
         }
-        encriptar(fd, argv[2]); // argv[2] es el mensaje
-    }
 
+        encriptar(fd, argv[2], strlen(argv[2])); // argv[2] es el mensaje
+        close(fd);
+    }
     // Termina la ejecución del programa.
+
     exit(EXIT_SUCCESS);
 }
